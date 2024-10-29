09715bcd451ae9de7ba6ca5e85d512b2912276e2
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-10-29 17:35:35 -0500

    Solucion del ejercicio Pitágoras

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#Escriba un programa que pida al usuario un entero de tres dígitos,
-#y entregue el número con los dígitos en orden inverso:
-#Ingrese numero: 345
-#543
-#Ingrese numero: 241
-#142
-
+#Pitágoras
+#Escriba un programa que reciba como entrada las longitudes de los dos catetos a
+# y b
+# de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c
+# del triangulo, dado por el teorema de Pitágoras: c2=a2+b2
+#Ingrese cateto a: 7
+#Ingrese cateto b: 5
+#La hipotenusa es 8.6023252670426267
+import math
 
 
 print(f""" 
@@ -20,30 +22,26 @@ name = input("    Hello, please enter your full name:  ")
 while True:
    
     print(f""" \n
-        Welcome back Mr/Ms {name}, this is a program for invert the order of your digits \n 
-        Please enter your digits: \n """) 
+        Welcome back Mr/Ms {name}, this is a program for calculate the hipotenuse in a right triangle \n 
+        Please enter your legs : \n """) 
     try :    
         
-        digits =   input("        ")
+        legA =   float(input("        leg a : "))
+        legB =   float(input("        leg a : "))
+
+        hipotenuse = math.sqrt((legA**2) + (legB**2))
+
+        print("The hipotenuse for the triangle is : ", hipotenuse)
+
         
-        if digits.isdigit() : #super util el isdigit(), tambien puede usarse isdecimal()
-        
 
-            invertDigits = digits[::-1] #el slicinng permite crear una subcadena de la cadena y el -1 para invertirla
-        
-            print("        Your invert digits is: ", invertDigits)
-
-        else :  
-
-            print("        Enter valid digits (nubers only)")
-
-        continueAsk = input( "\n    Do you want to invert again? (yes/no): " ).strip().lower()
+        continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
         
         if continueAsk != "yes" :
                 print("    Thaks for using the program. Goodbye! \n ")
                 break
     except :            
-        continueAsk = input( "\n    Do you want to invert again? (yes/no): " ).strip().lower()
+        continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
         
         if continueAsk != "yes" :
                 print("    Thaks for using the program. Goodbye! \n ")
