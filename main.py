323929e258972afae17a9f756e12f66689dd0095
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-10-29 20:11:22 -0500

    Solucion del ejercicio Parte decimal

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-#Escriba un programa que pregunte al usuario la hora actual t del reloj 
-# y un número entero de horas h,
-#  que indique qué hora marcará el reloj dentro de h horas:
+#Escriba un programa que entregue la parte decimal
+#de un número real ingresado por el usuario.
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43
-#En 43 horas, el reloj marcara las 6
+#Ingrese un numero: 4.5
+#0.5
+#Ingrese un numero: -1.19
+#0.19
+
 import math
 
 
@@ -24,26 +22,17 @@ name = input("    Hello, please enter your full name:  ")
 while True:
    
     print(f""" \n
-        Welcome back Mr/Ms {name}, this is a program for calculate the future time if you enter the actual time and the plus hours that you need to put in:  \n 
-         \n """) 
+        Welcome back Mr/Ms {name}, this is a program for calculate de decimal part of a number: \n 
+          """) 
     try :    
         
-        actualTime=   float(input("        Enter the actual time (12 hours format): "))
+        aNumber = float(input("        Please enter the number for the conversion : "))
         
-        if actualTime < 12 :
-            plusTime = float(input("        Enter the time that you need to add : "))
+        intNumber = int(aNumber)
+        decimalNumber = aNumber - intNumber 
+        rdecimalNumber = round(decimalNumber, 8)
 
-            finalTime = plusTime + actualTime
-
-            while finalTime >= 12:
-                finalTime -= 12
-
-            print(f"""        In {plusTime} hours, the clock will show {finalTime} o'clock """) 
-
-        else :
-            pass
-            
-        
+        print(f"        The decimal part of the number {aNumber} is : ", rdecimalNumber)
 
         continueAsk = input( "\n    Do you want to calculate again? (yes/no): " ).strip().lower()
         
